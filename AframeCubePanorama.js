99292ba9b374892183c3f1e1331083957fbff9cb
PepeDrago <peerdraeger@googlemail.com>
2017-01-31 23:12:51 +0100

    cleaned up ui trigger

--- AframeCubePanorama.js
+++ AframeCubePanorama.js
@@ -1,46 +1,120 @@
-$('.Close').click(function(){OnClose(this)});
-$('.btn').hover(function(){OnButtonHover(this)});
-
- $("#QRCodeWindow").addClass('hidden');
- $("#StartScreen").addClass('visible');
-
 //------------------------------
-
-
-function OnClose(_obj)
+var IsMobile      = false;
+var HeadSetActive = false;
+var StartScreen;
+var QRScreen;
+//------------------------------
+var PrevScreenState;
+//------------------------------
+const ScreenStates =
 {
-    var parentWindow = $("#"+$(_obj).attr("parentID"));
- 
-     $(parentWindow).fadeOut("slow",function()
+   LOADING     : 0,
+   STARTSCREEN : 1,
+   QRSCREEN    : 2,
+   MONOVIEW    : 3,
+   STEREOVIEW  : 4   
+}
+
+var Screenstate = {
+  current : 0,
+  set current (state)
+  {
+    ChangeScreen(PrevScreenState,"out");
+    current = state;  
+    ChangeScreen(state,"in");
+    PrevScreenState = state;
+  },
+  get current ()
+  {
+    return current;
+  }
+  
+}
+//------------------------------
+function Init()
+{
+ StartScreen = $("#StartScreen");
+ QRScreen    = $("#QRCodeWindow");
+
+ IsMobile      = AFRAME.utils.device.isMobile();
+ IsHeadSet     = AFRAME.utils.device.checkHeadsetConnected ();
+    
+ $('.btn').click(function(){OnBtnClicked(this)});
+ $('.btn').hover(function(){OnBtnHover(this)});  
+    
+ $(QRScreen).addClass('hidden');
+ $(StartScreen).addClass('visible');
+    
+ PrevScreenState = ScreenStates.LOADING;
+ Screenstate.current = ScreenStates.STARTSCREEN;  
+  
+}
+//------------------------------
+function ChangeScreen(_state,_direction)
+{
+    var screen = null;
+    
+    switch(_state)
+    {
+        case ScreenStates.STARTSCREEN:
+        screen = StartScreen;
+        break;
+        case ScreenStates.QRSCREEN:
+        screen = QRScreen;
+        break;
+        case ScreenStates.MONOVIEW:
+        StopAnimation();
+        break
+        case ScreenStates.STEREOVIEW:
+        StopAnimation();
+        break;
+        default:
+        console.log(PrevScreenState);
+        break;
+    } 
+    
+    if(_direction==="in")
+    {
+        $(screen).fadeIn("slow",function()
      {
-        ToggleWindow(parentWindow);
+        ToggleWindow(screen);
      });
-
-
+    }
+    else if(_direction==="out")
+    {
+       $(screen).fadeOut("slow",function()
+     {
+        ToggleWindow(screen);
+     });
+    }
+}
+//------------------------------
+function OnBtnClicked(_obj)
+{
     if($(_obj).attr("id")=="startBtn")
     {
-       $('#camAnimation').attr('mixin',"stopRotation");
+       Screenstate.current = ScreenStates.MONOVIEW;
     }
     if($(_obj).attr("id")=="vrBtn")
     {
-        if(AFRAME.utils.device.isMobile ()||AFRAME.utils.device.checkHeadsetConnected ())
+        if(IsMobile||IsHeadSet)
         {
-          $('#camAnimation').attr('mixin',"stopRotation");  
+          Screenstate.current = ScreenStates.STEREOVIEW;
           document.querySelector('a-scene').enterVR();  
         }
         else
         {
+            Screenstate.current = ScreenStates.QRSCREEN;
             ToggleWindow($("#QRCodeWindow"));       
         }    
     }
     if($(_obj).attr("id")=="backBtn")
     {
-       console.log("back to start");
+       Screenstate.current = ScreenStates.STARTSCREEN;
        ToggleWindow($("#StartScreen"));
-    }
-    
+    }  
 }
-
+//------------------------------
 function ToggleWindow(_window){
     if($(_window).attr('visible'))
     {
@@ -53,15 +127,18 @@ function ToggleWindow(_window){
         $(_window).addClass('visible');
     }
 }
-
-
-function OpenQRCodeWindow()
-{
-    
-}
-
-function OnButtonHover(_obj){
+//------------------------------
+function OnBtnHover(_obj){
     $(_obj).find(".Icon").toggleClass("inverted");
 }
-
-
+//------------------------------
+function StartAnimation()
+{
+    $('#camAnimation').attr('mixin',"camStartScreenRotation");
+}
+function StopAnimation()
+{
+    $('#camAnimation').attr('mixin',"stopRotation");
+}
+//------------------------------
+Init();
